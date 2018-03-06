3e5f469644b07dbec84f1a783bd0df9cba09164c
Mobil Trakya <mobiltrakya@Trakya.local>
2018-03-06 16:08:56 +0300

    Added objects to Storyboard.

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -21,7 +21,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(){
-        
+        let alert = UIAlertController(title: "Hello world", message: "This is my first app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
 
