73df245190e9edb2d290ed714f2ba9b4aa64ac1c
Mobil Trakya <mobiltrakya@Trakya.local>
2018-03-07 16:08:16 +0300

    Added functionality into slider.

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,10 +25,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello world", message: "This is my first app", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "The value of the slider now is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print("The value of the slider now is: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 }
 
