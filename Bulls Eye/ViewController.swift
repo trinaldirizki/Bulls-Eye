ebebedf78525549be8c235bfbde74c7444141a31
Mobil Trakya <mobiltrakya@Trakya.local>
2018-03-07 16:33:17 +0300

    Added startNewRound() method.

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue: Int = 0
     @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        currentValue = lroundf(slider.value)
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,16 +26,23 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert(){
-        let message = "The value of the slider now is: \(currentValue)"
+        let message = "The value of the slider now is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
-        print("The value of the slider now is: \(slider.value)")
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 }
 
